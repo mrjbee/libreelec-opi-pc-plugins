39293bc156f924e0af9a20b3a4e160b760cb075e
mrjbee <misterJBee@gmail.com>
2019-07-01 19:23:11 +0300

    Revert openssl

--- .github/build.sh
+++ .github/build.sh
@@ -93,12 +93,12 @@ git commit -m "Deploy to GitHub Pages: ${SHA} (Travis Build: $TRAVIS_BUILD_NUMBE
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${ENCRYPTED_IV_VAR}
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-# eval `ssh-agent -s`
+eval `ssh-agent -s`
 # Use stdin/stdout instead of key writing to disk
-# openssl aes-256-cbc -K $ENCRYPTION_LABEL -iv $ENCRYPTION_LABEL -in "$CWD/.github/deploy_key.enc" -d | ssh-add -
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in "$CWD/.github/deploy_key.enc" -d | ssh-add -
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
