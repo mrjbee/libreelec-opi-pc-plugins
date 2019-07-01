27e7ba6e57a1d40123f9aa54f97cfd593aa56fb8
mrjbee <misterJBee@gmail.com>
2019-07-01 18:53:00 +0300

    [HF] set vi/k  value

--- .github/build.sh
+++ .github/build.sh
@@ -93,8 +93,8 @@ git commit -m "Deploy to GitHub Pages: ${SHA} (Travis Build: $TRAVIS_BUILD_NUMBE
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+ENCRYPTED_KEY=${ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${ENCRYPTED_IV_VAR}
 
 eval `ssh-agent -s`
 # Use stdin/stdout instead of key writing to disk
